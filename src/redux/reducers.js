f4fd4765c896c8754f27be2ec443f51ebfc27a40
Andre Noberto <andre-noberto@hotmail.com>
2020-05-02 15:09:02 -0300

    Fixing typo

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 export default history => combineReducers({
-  rotuer: connectRouter(history),
+  router: connectRouter(history),
 });
